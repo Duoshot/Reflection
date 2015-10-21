00ed0df02026b59947342302b01771afd93a5c2a
Duoshot <obiwannabe1337@hotmail,.com>
2015-10-21 12:12:28 -0600

    v10 r5
    New testing for array. Removed array checking in fields. New method
    inspectMethod.

--- Inspector.java
+++ Inspector.java
@@ -18,43 +18,23 @@ public class Inspector {
 	{
 		Vector objectsToInspect = new Vector();
 		Class ObjClass = obj.getClass();
-
-		//System.out.println("	Inside inspector: " + obj + " (recursive = "+recursive+")");
-		
-		
-		//get class name
-		//String className = ObjClass.getName();
-		//System.out.println("	Current Class: " + className);
-		
-		//get first super class
-		//Class superClass = ObjClass.getSuperclass();
-		//System.out.println("	Super Class: " + superClass.getName());
-		
-		//if(ObjClass.isArray())
-		
-		//inspectInterfaces(ObjClass);
-		
-		//inspectMethods(ObjClass);
-
-		//inspectConstructors(ObjClass);
-		
-		//get fields
-		//type and modifiers
-		
-		
-	
-		//inspectFields(obj, ObjClass,objectsToInspect);
-		
-		//recursive = true;
-		
-		//if(recursive)
-		//    inspectFieldClasses( obj, ObjClass, objectsToInspect, recursive);
-		
 		inspect(obj, ObjClass, recursive);
 		   
 	}
 	
 	public void inspect(Object obj, Class ObjClass, boolean recursive)
+	{
+		if(ObjClass.isArray())
+		{
+			System.out.println("IT'S AN ARRAY");
+		}
+		else
+		{
+			inspectObject(obj, ObjClass, recursive);
+		}
+	}
+	
+	private void inspectObject(Object obj, Class ObjClass, boolean recursive)
 	{
 		Vector objectsToInspect = new Vector();
 		
@@ -94,11 +74,8 @@ public class Inspector {
 				inspect(obj, superClass, recursive);
 			}
 		}
-		
-		
-	
-	
 	}
+	
 
 	/*--------------------------------------------------------------------*/
 	private void inspectInterfaces(Class ObjClass)
@@ -129,35 +106,42 @@ public class Inspector {
 		System.out.println("\tMethod(s): ");
 		
 		Method[] methods = ObjClass.getDeclaredMethods();
-		for(int i = 0; i < methods.length; i++)
+		if(methods.length>0)
 		{
-			methods[i].setAccessible(true);
-			
-			//method name
-			System.out.println("\t\tName: " + methods[i].getName());
-			
-			//exceptions thrown
-			Class[] exception = methods[i].getExceptionTypes();
-			for(int j = 0; j < exception.length; j++)
+			for(int i = 0; i < methods.length; i++)
 			{
-				System.out.println("\t\t\tException thrown: " + exception[j].getName());
+				methods[i].setAccessible(true);
+			
+				//method name
+				System.out.println("\t\tName: " + methods[i].getName());
+			
+				//exceptions thrown
+				Class[] exception = methods[i].getExceptionTypes();
+				for(int j = 0; j < exception.length; j++)
+				{
+					System.out.println("\t\t\tException thrown: " + exception[j].getName());
+				}
+			
+				//parameter types
+				Class[] parameters = methods[i].getParameterTypes();
+				for(int k = 0; k < parameters.length; k++)
+				{
+					System.out.println("\t\t\tParameters: " + parameters[k].getName());
+				}
+			
+				//return type
+				Class returnName = methods[i].getReturnType();
+				System.out.println("\t\t\tReturn type: " + returnName.getName());
+			
+				//Modifier
+				int modifier = methods[i].getModifiers();
+				System.out.println("\t\t\tModifiers: " + Modifier.toString(modifier));
+			
 			}
-			
-			//parameter types
-			Class[] parameters = methods[i].getParameterTypes();
-			for(int k = 0; k < parameters.length; k++)
-			{
-				System.out.println("\t\t\tParameters: " + parameters[k].getName());
-			}
-			
-			//return type
-			Class returnName = methods[i].getReturnType();
-			System.out.println("\t\t\tReturn type: " + returnName.getName());
-			
-			//Modifier
-			int modifier = methods[i].getModifiers();
-			System.out.println("\t\t\tModifiers: " + Modifier.toString(modifier));
-			
+		}
+		else
+		{
+			System.out.println("\t\tThis class has no methods");
 		}
 	}
 	
@@ -169,23 +153,30 @@ public class Inspector {
 		
 		Constructor[] constructors = ObjClass.getDeclaredConstructors();
 		
-		for(int i = 0; i < constructors.length; i++)
+		if(constructors.length>0)
 		{
-			constructors[i].setAccessible(true);
-			
-			String cName = constructors[i].getName();
-			System.out.println("\t\tName: " + cName);
-			
-			//parameters
-			Class[] parameters = constructors[i].getParameterTypes();
-			for(int j = 0; j < parameters.length; j++)
+			for(int i = 0; i < constructors.length; i++)
 			{
-				System.out.println("\t\t\tParameters: " + parameters[j].getName());
-			}
-			//modifiers
-			int modifier = constructors[i].getModifiers();
-			System.out.println("\t\t\tModifiers: " + Modifier.toString(modifier));
+				constructors[i].setAccessible(true);
 			
+				String cName = constructors[i].getName();
+				System.out.println("\t\tName: " + cName);
+			
+				//parameters
+				Class[] parameters = constructors[i].getParameterTypes();
+				for(int j = 0; j < parameters.length; j++)
+				{
+					System.out.println("\t\t\tParameters: " + parameters[j].getName());
+				}
+				//modifiers
+				int modifier = constructors[i].getModifiers();
+				System.out.println("\t\t\tModifiers: " + Modifier.toString(modifier));
+			
+			}
+		}
+		else
+		{
+			System.out.println("\t\tThis class has no Constructors");
 		}
 	}
 	
@@ -209,7 +200,7 @@ public class Inspector {
 	    	try
 			{
 	    		System.out.println("******************");
-	    		inspect( f.get(obj) , recursive);
+	    		inspect( f.get(obj) , f.get(obj).getClass(), recursive);
 	    		
 			}
 	    	catch(Exception exp) { exp.printStackTrace(); }
@@ -219,48 +210,44 @@ public class Inspector {
 	private void inspectFields(Object obj, Class ObjClass, Vector objectsToInspect)
 	{
 		Field[] fields = ObjClass.getDeclaredFields();
-		for(int i = 0; i < fields.length; i++)
+		
+		if(fields.length > 0)
 		{
-			fields[i].setAccessible(true);
-			
-	    	if(! fields[i].getType().isPrimitive() ) 
-	    		objectsToInspect.addElement( fields[i] );
-	    	
-	    	System.out.println("\t\tName: " + fields[i].getName());
-	    	
-	    	System.out.println("\t\t\tDeclaring Class: " + fields[i].getDeclaringClass().getName());
-	    	
-	    	Class fType = fields[i].getType();
-			System.out.println("\t\t\tType: " + fType.getName());
-			
-			int fMod = fields[i].getModifiers();
-			System.out.println("\t\t\tModifiers: " + Modifier.toString(fMod));
-			
-			Object value = null;
-	    	try
+		
+			for(int i = 0; i < fields.length; i++)
 			{
-	    		value = fields[i].get(obj);
-			}
-	    	catch(Exception e) {}    
+				fields[i].setAccessible(true);
+			
+				if(! fields[i].getType().isPrimitive() ) 
+					objectsToInspect.addElement( fields[i] );
+	    	
+				System.out.println("\t\tName: " + fields[i].getName());
+	    	
+				System.out.println("\t\t\tDeclaring Class: " + fields[i].getDeclaringClass().getName());
+	    	
+				Class fType = fields[i].getType();
+				System.out.println("\t\t\tType: " + fType.getName());
+			
+				int fMod = fields[i].getModifiers();
+				System.out.println("\t\t\tModifiers: " + Modifier.toString(fMod));
+			
+				Object value = null;
+				try
+				{
+					value = fields[i].get(obj);
+				}
+				catch(Exception e) {}    
 //	    	
-	    	System.out.println("\t\t\tValue: " + value);
-//	    	if(fType.isArray())
-//			{
-//					System.out.println("\t\t\tArray Length: " + Array.getLength(value));
-//
-//					for(int k = 0; k < Array.getLength(value); k++)
-//				{
-//						System.out.println("\t\t\t\tArray Element " + k + ": "+ Array.get(value, k));
-//					}
-//				}
-//				else
-//					System.out.println("\t\t\tValue: " + value);
-//				
-//			}
+				System.out.println("\t\t\tValue: " + value);
 
+			}
 		}
-	    //if(ObjClass.getSuperclass() != null)
-	    	//	inspectFields(obj, ObjClass.getSuperclass() , objectsToInspect);
+		else
+		{
+			System.out.println("\t\tThis class has no Fields");
+		}
+			//if(ObjClass.getSuperclass() != null)
+			//	inspectFields(obj, ObjClass.getSuperclass() , objectsToInspect);
 	}
 
 }
